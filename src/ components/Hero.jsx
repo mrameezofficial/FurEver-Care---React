780439bf07841a054ce6f9a8cf910105ea52b193
Muhammad Rameez <mrameezofficial@gmail.com>
2025-09-10 18:20:40 +0500

    hero-update

--- src/ components/Hero.jsx
+++ src/ components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = ({pageTitle}) => {
   return (
-    <section className='inner-banner '>
-      <div className='row'>
+    <section className='inner-banner h-[40vh] bg-[var(--green)]'>
+      <div className='row w-[var(--globeWidth)] m-auto'>
         <div className='col-12'>
-            <h1>{pageTitle}</h1>
+            <h1 className='text-center'>{pageTitle}</h1>
         </div>
       </div>
     </section>
