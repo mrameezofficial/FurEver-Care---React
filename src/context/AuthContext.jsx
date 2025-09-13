becf88c89a7f3f878784808e074a3020d5fd58a3
Muhammad Rameez <mrameezofficial@gmail.com>
2025-09-13 11:09:45 +0500

    header new

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,34 +3,31 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [category, setCategory] = useState(null);
   const [name, setName] = useState("");
- const [userName,setUserName]= useState("DUMMY USER")
+  const [category, setCategory] = useState(null);
+  const [userName, setUserName] = useState("");
 
-console.log("asdasdasda",name)
-useEffect(()=>{
-  // localStorage.setItem("name",name);
- let a= localStorage.getItem("name")
-setUserName(a)
-},[name,category])
+// useEffect(()=>{
+//   // localStorage.setItem("name",name);
+//  let a= localStorage.getItem("name")
+// setUserName(a)
+// },[name,category])
+useEffect(() => {
+    const storedCategory = localStorage.getItem("category");
+    const storedUserName = localStorage.getItem("userName");
+    if (storedCategory) setCategory(storedCategory);
+    if (storedUserName) setUserName(storedUserName);
+  }, []);
 
-  // useEffect(() => {
-  //   const savedCategory = localStorage.getItem("category");
-  //   const savedName = localStorage.getItem("name");
-  //   if (savedCategory) setCategory(savedCategory);
-  //   if (savedName) setName(savedName);
-  // }, [category, name]);
 
-  // useEffect(() => {
-  //   if (category) localStorage.setItem("category", category);
-  //   if (name) localStorage.setItem("name", name);
-  // }, [category, name]);
+
 
   return (
-    <AuthContext.Provider value={{ category, setCategory, name, setName,userName }}>
+    <AuthContext.Provider value={{ category, setCategory, userName, setUserName }}>
       {children}
     </AuthContext.Provider>
   );
+
 };
 
 export const useAuth = () => useContext(AuthContext);
